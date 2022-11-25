a30df6b9d4e6b3b0a01b1c6d3f01bba4b4c4d6d5
arunkumars-itflex <arunkumar.s@itflexsolutions.com>
2022-11-25 12:59:25 +0530

    navigation menu

--- app/Controllers/Api/Navigationmenu/Index.php
+++ app/Controllers/Api/Navigationmenu/Index.php
@@ -15,6 +15,7 @@ class Index extends BaseController
 
     public function index()
     {
+        
         $post       = $this->request->getPost();  //print_r($post);die;
         $validation = \Config\Services::validation();
 
