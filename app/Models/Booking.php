08732066f67ff31d30b744d3260a99c0374d9851
nanthakumar5 <nanthakumar@itflexsolutions.com>
2022-12-23 13:01:11 +0530

    B

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -80,7 +80,7 @@ class Booking extends BaseModel
 		
 		if(isset($requestdata['gtenddate'])){
 			$query->groupStart();
-				$query->groupStart()->where(['b.type' => '1', 'e.type' => '1'])->groupStart()->where(['e.end_date >=' => date('Y-m-d', strtotime($requestdata['ltenddate']))])->orWhere(['b.check_out >=' => date('Y-m-d', strtotime($requestdata['ltenddate']))])->groupEnd()->groupEnd();
+				$query->groupStart()->where(['b.type' => '1', 'e.type' => '1'])->groupStart()->where(['e.end_date >=' => date('Y-m-d', strtotime($requestdata['gtenddate']))])->orWhere(['b.check_out >=' => date('Y-m-d', strtotime($requestdata['gtenddate']))])->groupEnd()->groupEnd();
 				$query->orGroupStart()->where(['b.type' => '2', 'e.type' => '2', 'b.check_out >=' => date('Y-m-d', strtotime($requestdata['gtenddate']))])->groupEnd();
 			$query->groupEnd();
 		}
