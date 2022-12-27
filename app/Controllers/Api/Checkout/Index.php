648220fb79951b0082c5d2097a8ddcbd45120f17
muthulakshmi <muthulakshmi@itflexsolutions.com>
2022-12-27 16:42:11 +0530

    checkout cart

--- app/Controllers/Api/Checkout/Index.php
+++ app/Controllers/Api/Checkout/Index.php
@@ -44,113 +44,141 @@ class Index extends BaseController
 			$result         = $this->cart->getCart('all', ['cart', 'event', 'barn', 'stall', 'product', 'tax'], $condition);
 
 		if($result){
-			$setting 				= getSettings();
-			$cartreservedtime		= $this->cart->getReserved($setting['cartreservedtime']);
-			$timer					= $cartreservedtime ? $cartreservedtime : '';
-			$count					= count($result);
-			
-			$event_id 				= array_unique(array_column($result, 'event_id'))[0];
-			$event_name 			= array_unique(array_column($result, 'eventname'))[0];
-			$event_location 		= array_unique(array_column($result, 'eventlocation'))[0];
-			$event_description 		= array_unique(array_column($result, 'eventdescription'))[0];
-			$cleaning_fee 			= array_unique(array_column($result, 'eventcleaningfee'))[0];
-		    $check_in       		= formatdate(array_unique(array_column($result, 'check_in'))[0], 1);
-		    $check_out      		= formatdate(array_unique(array_column($result, 'check_out'))[0], 1);
-		    $start          		= strtotime(array_unique(array_column($result, 'check_in'))[0]);
-			$end            		= strtotime(array_unique(array_column($result, 'check_out'))[0]);
-			$daydiff           		= ceil(abs($start - $end) / 86400);
-			$interval           	= $daydiff==0 ? 1 : $daydiff;
-			$type          			= array_unique(array_column($result, 'type'))[0];
-			$tax 					= isset($result[0]['tax']['tax_price']) ? $result[0]['tax']['tax_price'] :'0';
+		$setting 				= getSettings();
+		$cartreservedtime		= $cart->getReserved($setting['cartreservedtime']);
+		$timer					= $cartreservedtime ? strtotime($cartreservedtime) : '';
+		$count					= count($result);
+		
+		$event_id 				= array_unique(array_column($result, 'event_id'))[0];
+		$event_name 			= array_unique(array_column($result, 'eventname'))[0];
+		$event_location 		= array_unique(array_column($result, 'eventlocation'))[0];
+		$event_description 		= array_unique(array_column($result, 'eventdescription'))[0];
+		$cleaning_fee 			= array_unique(array_column($result, 'eventcleaningfee'))[0];
+	    $check_in       		= formatdate(array_unique(array_column($result, 'check_in'))[0], 1);
+	    $check_out      		= formatdate(array_unique(array_column($result, 'check_out'))[0], 1);
+	    $start          		= strtotime(array_unique(array_column($result, 'check_in'))[0]);
+		$end            		= strtotime(array_unique(array_column($result, 'check_out'))[0]);
+		$daydiff           		= ceil(abs($start - $end) / 86400);
+		$interval           	= $daydiff==0 ? 1 : $daydiff;
+		$type          			= array_unique(array_column($result, 'type'))[0];
+		$tax 					= isset($result[0]['tax']['tax_price']) ? $result[0]['tax']['tax_price'] :'0';
 
-			$barnstall = $rvbarnstall = $feed =  $shaving = [];
-			$price = 0;
-			
-			foreach ($result as $res) {
-				$singleprice = $res['price'];
-					
-				if($res['flag']=='1' || $res['flag']=='2'){				
-					$singlechargingid 	= $res['chargingid'];
-					$singlepricetype 	= $res['price_type'];
-					
-					if($singlepricetype==0){
-						$intervalday = $interval;
-						$intervalday = ($intervalday%7==0) ? ($intervalday/7) : $intervalday;
-						$intervalday = ($intervalday%30==0) ? ($intervalday/30) : $intervalday;
-						if($singlechargingid=='4') $intervalday = 1;
-							
-						$singletotal = $singlechargingid=='4' ?  $singleprice : $singleprice * $intervalday;
-					}else{
-						$intervalday = $interval;
-						if($singlepricetype=='2') 		$intervalday = ceil($intervalday/7);
-						elseif($singlepricetype=='3') 	$intervalday = ceil($intervalday/30);
-						elseif($singlepricetype=='4') 	$intervalday = 1;
-						elseif($singlepricetype=='5') 	$intervalday = 1;
+		$barnstall = $rvbarnstall = $feed =  $shaving = [];
+		$price = 0;
+		
+		foreach ($result as $res) {
+			$singleprice = $res['price'];
+				
+			if($res['flag']=='1' || $res['flag']=='2'){				
+				$singlechargingid 	= $res['chargingid'];
+				$singlepricetype 	= $res['price_type'];
+				
+				if($singlepricetype==0){
+					$intervalday = $interval;
+					$intervalday = ($intervalday%7==0) ? ($intervalday/7) : $intervalday;
+					$intervalday = ($intervalday%30==0) ? ($intervalday/30) : $intervalday;
+					if($singlechargingid=='4') $intervalday = 1;
 						
-						if($singlepricetype=='1') 		$singletotal = $singleprice * $intervalday;
-						elseif($singlepricetype=='2') 	$singletotal = $singleprice * $intervalday;
-						elseif($singlepricetype=='3') 	$singletotal = $singleprice * $intervalday;
-						elseif($singlepricetype=='4') 	$singletotal = $singleprice;
-						elseif($singlepricetype=='5') 	$singletotal = $singleprice;
+					$singletotal = $singlechargingid=='4' ?  $singleprice : $singleprice * $intervalday;
+				}else{
+					$intervalday 	= $interval;					
+					$intervalcalc 	= $interval;	
+					
+					if(in_array($singlepricetype, ['1', '2', '3'])){
+						$singleprice 	= 0;
+						$singletotal 	= 0;
+						$mwnpricelist	= explode(',', $res['mwn_price']);
+						
+						$mwnprice = $mwninterval = $mwntotal = [0, 0, 0];
+						
+						$monthcalc = intdiv($intervalcalc, 30);						
+						if($monthcalc > 0){
+							$mwnprice[0] = $mwnpricelist[0];
+							$mwninterval[0] = $monthcalc;
+							$mwntotal[0] = $mwnprice[0] * $mwninterval[0];
+							$singletotal += $mwntotal[0];
+							$intervalcalc = $intervalcalc - (30 * $monthcalc);
+						}
+							
+						$weekcalc = intdiv($intervalcalc, 7);
+						if($weekcalc > 0){
+							$mwnprice[1] = $mwnpricelist[1];
+							$mwninterval[1] = $weekcalc;
+							$mwntotal[1] = $mwnprice[1] * $mwninterval[1];
+							$singletotal += $mwntotal[1];
+							$intervalcalc = $intervalcalc - (7 * $weekcalc);
+						}
+						
+						if($intervalcalc > 0){
+							$mwnprice[2] = $mwnpricelist[2];
+							$mwninterval[2] = $intervalcalc;
+							$mwntotal[2] = $mwnprice[2] * $mwninterval[2];
+							$singletotal += $mwntotal[2];
+						}
+					}else{
+						$intervalday = 1;
+						$singletotal = $singleprice;
 					}
-					
-					$barnrvdata = [
-						'barn_id' 				=> $res['barn_id'], 
-						'barn_name' 			=> $res['barnname'], 
-						'stall_id' 				=> $res['stall_id'],
-						'stall_name' 			=> $res['stallname'],
-						'subscriptionprice' 	=> $res['subscription_price'],
-						'price' 				=> $singleprice,
-						'pricetype' 			=> $singlepricetype,
-						'chargingid' 			=> $singlechargingid,
-						'interval' 				=> $interval,
-						'intervalday' 			=> $intervalday,
-						'total' 				=> $singletotal
-					];
-					
-					if($res['flag']=='1') 		$barnstall[] = $barnrvdata;
-					elseif($res['flag']=='2')	$rvbarnstall[] = $barnrvdata;
-					
-					$price += $singletotal;
-				}else if($res['flag']=='3' || $res['flag']=='4'){
-					$singlequantity = $res['quantity'];
-					$singletotal = $singleprice * $singlequantity;
-					
-					$feedshavingdata = [
-						'product_id'	=> $res['product_id'], 
-						'product_name'	=> $res['productname'], 
-						'price' 		=> $singleprice,
-						'quantity' 		=> $singlequantity,
-						'total' 		=> $singletotal
-					];
-					
-					if($res['flag']=='3') 		$feed[] = $feedshavingdata;
-					elseif($res['flag']=='4')	$shaving[] = $feedshavingdata;
-					
-					$price += $singletotal;
 				}
+				
+				$barnrvdata = [
+					'barn_id' 				=> $res['barn_id'], 
+					'barn_name' 			=> $res['barnname'], 
+					'stall_id' 				=> $res['stall_id'],
+					'stall_name' 			=> $res['stallname'],
+					'subscriptionprice' 	=> $res['subscription_price'],
+					'price' 				=> $singleprice,
+					'pricetype' 			=> $singlepricetype,
+					'chargingid' 			=> $singlechargingid,
+					'interval' 				=> $interval,
+					'intervalday' 			=> $intervalday,
+					'total' 				=> $singletotal,
+					'mwn_price' 			=> isset($mwnprice) ? $mwnprice : '',
+					'mwn_interval' 			=> isset($mwninterval) ? $mwninterval : '0',
+					'mwn_total' 			=> isset($mwntotal) ? $mwntotal : '0',
+				];
+				
+				if($res['flag']=='1') 		$barnstall[] = $barnrvdata;
+				elseif($res['flag']=='2')	$rvbarnstall[] = $barnrvdata;
+				
+				$price += $singletotal;
+			}else if($res['flag']=='3' || $res['flag']=='4'){
+				$singlequantity = $res['quantity'];
+				$singletotal = $singleprice * $singlequantity;
+				
+				$feedshavingdata = [
+					'product_id'	=> $res['product_id'], 
+					'product_name'	=> $res['productname'], 
+					'price' 		=> $singleprice,
+					'quantity' 		=> $singlequantity,
+					'total' 		=> $singletotal
+				];
+				
+				if($res['flag']=='3') 		$feed[] = $feedshavingdata;
+				elseif($res['flag']=='4')	$shaving[] = $feedshavingdata;
+				
+				$price += $singletotal;
 			}
+		}
+		 
+		$transactionfee     => number_format((($setting['transactionfee'] / 100) * $price), 2);
+		$barnstallcolumn = array_column($barnstall, 'barn_id');
+		array_multisort($barnstallcolumn, SORT_ASC, $barnstall);
+		$rvbarnstallcolumn = array_column($rvbarnstall, 'barn_id');
+		array_multisort($rvbarnstallcolumn, SORT_ASC, $rvbarnstall);
+		$feedcolumn = array_column($feed, 'product_id');
+		array_multisort($feedcolumn, SORT_ASC, $feed);
+		$shavingcolumn = array_column($shaving, 'product_id');
+		array_multisort($shavingcolumn, SORT_ASC, $shaving);
 
-
-			$transactionfee = number_format((($setting['transactionfee'] / 100) * $price), 2);
-			$totaldue = $transactionfee+$count;
-			$barnstallcolumn = array_column($barnstall, 'barn_id');
-			array_multisort($barnstallcolumn, SORT_ASC, $barnstall);
-			$rvbarnstallcolumn = array_column($rvbarnstall, 'barn_id');
-			array_multisort($rvbarnstallcolumn, SORT_ASC, $rvbarnstall);
-			$feedcolumn = array_column($feed, 'product_id');
-			array_multisort($feedcolumn, SORT_ASC, $feed);
-			$shavingcolumn = array_column($shaving, 'product_id');
-			array_multisort($shavingcolumn, SORT_ASC, $shaving);
 			$resultdata = [
 				'event_id'			=> $event_id, 
 				'event_name'		=> $event_name, 
 				'event_tax'			=> $tax, 
 				'event_location' 	=> $event_location, 
 				'event_description' => $event_description, 
-				'cleaning_fee' 		=> $cleaning_fee, 
+				'cleaning_fee' 		=> $cleaning_fee,
 				'transactionfee' 	=> $transactionfee,
-				'totaldue' 			=> $totaldue,
 				'barnstall'			=> $barnstall,
 				'rvbarnstall'		=> $rvbarnstall, 
 				'feed'				=> $feed, 
@@ -161,7 +189,7 @@ class Index extends BaseController
 				'price' 			=> $price,
 				'type' 				=> $type,
 				'timer' 			=> $timer,
-				'count' 			=> $count,
+				'count' 			=> $count
 				'paymentmethod' 	=> $paymentmethod,
 				'stripepublickey' 	=> $setting['stripepublickey'],
 			];
@@ -171,7 +199,9 @@ class Index extends BaseController
 			} else {
 	            $json = ['0', 'No Record(s) Found', []];
 	        }
-	    }
+	    }else {
+	            $json = ['0', 'No Record(s) Found', []];
+	        }
 		}else {
 	            $json = ['0', $validation->getErrors(), []];
 	        }
